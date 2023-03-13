318a2a09e48d2e66b7ef48ef31066a1fce19dcce
Allan dos Santos Pinto <67328283+allancont@users.noreply.github.com>
2023-03-13 11:43:10 -0300

    Update ds23_noget.py

--- ds23_noget.py
+++ ds23_noget.py
@@ -3,34 +3,25 @@ import pandas as pd
 import geopy.distance
 import numpy as np
 import folium
-from streamlit_folium import st_folium
 from folium.plugins import HeatMap
 from streamlit_folium import folium_static
 from folium.plugins import MarkerCluster, HeatMap
-# import locale
-# import matplotlib.pyplot as plt
-# from matplotlib.dates import DateFormatter
-# locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
 header = st.container()
 dataset = st.container()
 features = st.container()
 model_training  = st.container()
 
-# st.markdown(
-#     """
-#     <style>
-#     .main {
-#     background-color: #F5F5F5;
-#     }
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-# )
-
+# Função para ler os dados
 @st.cache_data
-def get_data(filename):
-    df = pd.read_excel(filename)
-    return df
+def load_data(file_data):
+    try:
+        return pd.read_csv(file_data, dtype=dtypes,decimal=',',sep=';')
+    except:
+        return pd.read_csv(file_data, dtype=dtypes,decimal=',',sep=';',parse_dates=['periodo'])
+
+file_path_vendas = "vendas.csv" #"data/vendas.csv"
+file_path_vendas_predita2 = "vendas_predita2.csv"#"data/vendas_predita2.csv"
 
 with header:
     st.title ('Estudo de viabilidade econômica DS-23')
@@ -45,34 +36,29 @@ with dataset:
     st.write("Em seguida foram agregadas informações oficiais sobre as características dos municípios de Minas Gerais e Espírito Santo originadas de fontes oficiais do IBGE, CAGED e IPEA, totalizando 252 tipos diferentes de dados quantitativos e qualitativos sobre a população em diversos períodos.")
     st.write("Através o método *StepWise* foram selecionadas cerca de 16 variáveis para treinamento do modelo estatístico relacionadas a:")
     lista = ["Taxa de envelhecimento", "Educação", "Frequência escolar","% População rural", "IDH"]
-    # 'Taxa de envelhecimento',
-    # '% da renda apropriada pelos 20% mais pobres',
-    # '% da população em domicílios com coleta de lixo',
-    # '% de crianças de 11 a 14 anos na escola',
-    # '% dos ocupados na indústria de transformação',
-    # 'Taxa de frequência ensino médio',
-    # '% de crianças em domicílios em que ninguém tem fundamental completo',
-    # '% de 25 anos ou mais com superior completo',
-    # '% de 15 a 17 anos na escola',
-    # 'Taxa de atividade 15 a 17 anos',
-    # 'Taxa de analfabetismo 11 a 14 anos',    
-    # '% população rural',
-    # 'Taxa de frequência bruta ao básico',
-    # 'IDHM']
+
     for item in lista:
         st.write("- " + item)
-    df = get_data("C:/Users/Allan/OneDrive/Área de Trabalho/DS23/data/vendas_predita2.xlsx")#pd.read_excel("C:/Users/Allan/OneDrive/Área de Trabalho/DS23/data/vendas_predita.xlsx")
-    df['cod_IBGE'].astype(str)
-    # st.write(df.sample(5))
-    df_vendas = get_data("C:/Users/Allan/OneDrive/Área de Trabalho/DS23/data/vendas.xlsx")#pd.read_excel("C:/Users/Allan/OneDrive/Área de Trabalho/DS23/data/vendas.xlsx")
-    df_vendas = df_vendas[['Cidade','UF','venda_total','periodo']]
+    
+    
+    dtypes = {"cod_IBGE": str, "cidade": str, "UF": str,"latitude": float,"longitude": float,"venda_predita": float }
+    # df = pd.read_csv(file_path_vendas_predita2,dtype=dtypes,decimal=',',sep=';')
+    df = load_data(file_path_vendas_preditas2)
+    dtypes = {"Cidade": str, "UF": str,"venda_total": float,"periodo": str  ,"venda_predita": float }
+    usecols = ["Cidade", "UF","venda_total",'periodo']
+    df_vendas = load_data(file_path_vendas)
+    df_vendas['periodo'] = pd.to_datetime(df_vendas['periodo'])
+    df_vendas['mes_ano'] = df_vendas['periodo'].dt.strftime('%Y-%m')
+
     df_vendas['Cidade']=df_vendas['Cidade'].str.upper()
-    df_soma = df_vendas.groupby('periodo')['venda_total'].sum()
-    # st.write(df_vendas.sample(5))
-    st.header('Vendas mensais no período')
+    df_soma = df_vendas.groupby('mes_ano')['venda_total'].sum()
+    
+    st.markdown('**Vendas mensais utilizadas no período**')
     st.bar_chart(df_soma,use_container_width=True,width=5)
-    
-    
+
+
+
+
 with features:
     st.header('Metodologia Aplicada')
     st.write('Para o conjunto de dados obtido foi utilizado o algorítmo Random Forest.')
@@ -82,14 +68,12 @@ with model_training:
     st.header('Simulações')
     st.write('A seguir poderão ser feitas várias simulações de venda em cidades nos estados de **MG** e **ES**.')
     sel_col, disp_col = st.columns(2) 
-    # Criar um widget selectbox com as opções de cidades
-    # Criar uma lista com as 900 cidades
-    cidades = df.cidade.tolist()    
+    cidades = df.cidade.tolist()  
+
     with sel_col:
         cidade_selecionada = st.selectbox('Selecione uma cidade:', cidades)
         km_dist=sel_col.slider("Qual o raio em km você deseja obter para compor a região da cidade pesquisada?",min_value=10,max_value=200,value=20,step=10)
-        # st.write('Raio selecionado:', km_dist,'km')
-    # st.write('Qual o raio em km você deseja obter para compor a região da cidade pesquisada?')
+        
     filtro = (df['cidade'] == cidade_selecionada)
     lat_ref=np.asarray(df[filtro].latitude)[0]
     lon_ref=np.asarray(df[filtro].longitude)[0]
@@ -102,67 +86,37 @@ with model_training:
     df_km = df[(df["distancia"] <= km_dist)&(df["distancia"] >= 0)&(df["cod_IBGE"]!= '313120')].copy()
     df_km['distancia'] = round(df_km['distancia'],0)
     # print(f'\nForam encontradas {df_km.cidade.value_counts().sum()} cidades num raio de {km_dist} km de {cidade_selecionada}')
-    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
     soma_vendas = df_km['venda_predita'].sum()
-    valor_formatado = locale.currency(soma_vendas, grouping=True)
-    print('A soma das vendas preditas para a região é de', valor_formatado)
-
+    
     df_pesq=df_km[["cidade", "distancia",'venda_predita']].sort_values('venda_predita',ascending=True)
-    df_pesq['venda_predita']=round(df_pesq['venda_predita'],0)
-    df_pesq = df_pesq.rename(columns={'venda_predita': 'venda prevista'})
-    # df_pesq['venda_predita'] = df_pesq['venda_predita'].apply(lambda x: locale.currency(x, grouping=True))
-    total = df_pesq['venda prevista'].sum()
-    total_formatado = locale.currency(total, grouping=True)
+    df_pesq['venda prevista'] = df_pesq['venda_predita'].apply(lambda x: '{:,.0f}'.format(x).replace(',', '.'))
+    
+    total = df_pesq['venda_predita'].sum()
+    # total_formatado = locale.currency(total, grouping=True)
     # st.write('Total:', total_formatado)
     # with sel_col:
 
-
-    with disp_col:        
-        st.write(df_pesq.set_index('cidade'))
-        
-    with sel_col:
-        st.markdown(f"**Venda total  projetada: {total_formatado}**")
-    #Criando o objeto do mapa
-    mapa = folium.Map(location=[lat_ref, lon_ref], zoom_start=6)
-    data=df[['latitude', 'longitude', 'venda_predita']].copy()
-    ipa_loc=(data[(data['latitude']==-19.7992)&(data['longitude']==-41.7164)].index[0])
-    data.drop(ipa_loc,inplace=True)
-    st.markdown(f"**Mapa de vendas projetadas por região num raio de {km_dist} km**")
-    # # Adicione um mapa de calor com base nos dados de vendas
-    # heatmap = HeatMap(data=data.values.tolist(),
-    #                   name='Venda projetada',
-    #                   control=True,
-    #                   show=True,
-    #                   min_opacity=0.30,
-    #                   radius=20)
-    # heatmap.add_to(mapa)
-    # # Criar um objeto MarkerCluster para agrupar os marcadores próximos
-    # marker_cluster = MarkerCluster(max_cluster_radius=km_dist)
-
-    # # Adicionar marcadores personalizados com os valores de 'venda_predita'
-    # for lat, lon, venda_predita in data.values.tolist():
-    #     folium.Marker(location=[lat, lon],
-    #                   icon=folium.Icon(color='green', icon='info-sign'),
-    #                   tooltip=f'Venda Prevista: R$ {venda_predita:.2f}').add_to(marker_cluster)
-
-    # # Adicionar o objeto MarkerCluster ao mapa
-    # marker_cluster.add_to(mapa)
-
-    # # Adicionar um controle de camadas ao mapa
-    # folium.LayerControl().add_to(mapa)
-
-    # # Exibir o mapa com o heatmap e os marcadores no Streamlit
-    # folium_static(mapa)
-
-
-    #Criando o objeto do mapa
-    mapa2 = folium.Map(location=[lat_ref, lon_ref], zoom_start=6)
-    data=df[['latitude', 'longitude', 'venda_predita']].copy()
-    ipa_loc=(data[(data['latitude']==-19.7992)&(data['longitude']==-41.7164)].index[0])
-    data.drop(ipa_loc,inplace=True)
     
+    with disp_col:        
+        st.write(df_pesq[['cidade','distancia','venda prevista']].set_index('cidade'))
+    with sel_col:
+        st.markdown('**Venda total projetada na região: R$ {:,.2f}**'.format(total).replace(',', '.'))
+      
+    st.markdown(f"**Mapa de vendas projetadas por região num raio de {km_dist} km**")
+
+        
+
+    #Criando o objeto do mapa
+    # st.write(df.sample(3))
+    mapa2 = folium.Map(location=[-19.912998, -43.940933], zoom_start=6)
+    data=df[['cidade','latitude', 'longitude', 'venda_predita']]#.copy()
+    ipa_loc=(data[(data['latitude']==-19.7992)&(data['longitude']==-41.7164)].index[0])
+    data.drop(ipa_loc,inplace=True)
+
     # Adicione um mapa de calor com base nos dados de vendas
-    heatmap = HeatMap(data=data.values.tolist(),
+    # st.write(data.head(2).to_dict())
+    dt1=data[['latitude', 'longitude', 'venda_predita']]
+    heatmap = HeatMap(data=dt1.values.tolist(),
                       name='Venda projetada',
                       control=True,
                       show=True,
@@ -176,21 +130,28 @@ with model_training:
         return f'Soma de Vendas Previstas: R$ {soma:,.2f}'
 
     # Criar um objeto MarkerCluster para agrupar os marcadores próximos
-    marker_cluster = MarkerCluster(max_cluster_radius=km_dist)
-
+    # marker_cluster = MarkerCluster(max_cluster_radius=km_dist) #cluster escolhilho pelo usuário
+    marker_cluster = MarkerCluster(max_cluster_radius=km_dist) #cluster fixo
+    
     # Adicionar marcadores personalizados com os valores de 'venda_predita'
-    for lat, lon, venda_predita in data.values.tolist():
+    for lat, lon, venda_predita in dt1.values.tolist(): #for lat, lon, venda_predita in data.values.tolist():
         folium.Marker(location=[lat, lon], 
                     icon=None,
-                    tooltip=f'Venda Prevista: R$ {float(venda_predita):.2f}').add_to(marker_cluster)
-
+                    # popup=cidade("Mom & Pop Arrow Shop >>", parse_html=True),
+                    tooltip=f'R$ {venda_predita:,.0f}'.replace(',', '.')
+                    ).add_to(marker_cluster)
+                        
     marker_cluster.add_to(mapa2)
 
     # Adicionar um controle de camadas para exibir a soma das vendas previstas nos clusters
     folium.map.LayerControl(collapsed=False, overlay=True, control=False, position='topright').add_to(mapa2)
     marker_cluster.add_child(folium.map.Popup(soma_venda_predita))
 
-
+    
     # Exibir o mapa com o heatmap e os marcadores no Streamlit
     folium_static(mapa2)
 
+    
+
+
+    
